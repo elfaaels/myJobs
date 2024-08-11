966081fb0f8df79b92456f0c4dcafadd91026d94
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-08-11 10:02:23 +0700

    feat: add onRefresh callback

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -31,7 +31,11 @@ const JobDetails = () => {
   const [activeTab, setActiveTab] = useState(tabs[0]);
   const [ refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {}
+  const onRefresh = () => {
+    setRefreshing(true)
+    refetch()
+    setRefreshing(false)
+  }
 
   const displayTabContent = () => {
     switch (activeTab) {
